91188bbd00814cb9e3efa6ab74558469aa6b6c3a
Guillaume MANSUTTI <guillaume.mansutti@dodo.fr>
2025-06-02 18:13:43 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Digidex</h1>
+  <p>Filter Text: {{ filterText }}</p>
+  <input type="text" v-model="filterText"/>
+  <ul>
+    <li v-for="(digimon, index) in filteredDigiList" :key="`digi-${index}`">
+      # Name: {{ digimon.name }} - Level: {{ digimon.level }} #
+      <!-- <img :src="digimon.img"/> -->
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { ref, computed, onMounted } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const digiList = ref([])
+const filterText = ref('')
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+const filteredDigiList = computed(() => 
+  digiList.value.filter(digimon => 
+    digimon.name.includes(filterText.value)
+  )
+)
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+onMounted(async() => {
+  const digiData = await fetch('https://digimon-api.vercel.app/api/digimon').then( response => response.json()
+  )
+  digiList.value = digiData;
+})
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
