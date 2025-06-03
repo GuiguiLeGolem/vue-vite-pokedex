fac6e010fadb03accabc8ad989cda7bd304567c7
Guillaume MANSUTTI <guillaume.mansutti@dodo.fr>
2025-06-03 15:22:09 +0200

    added: remplacer la liste par un composant custom avec des props pour définir les attributs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Digidex</h1>
+  <h1>I don't know</h1>
   <p>Filter Text: {{ filterText }}</p>
   <input type="text" v-model="filterText"/>
   <div class="relative">
@@ -8,7 +8,8 @@
     </Transition>
   </div>
   <TransitionGroup name="loadingelement" mode="out-in" tag="ul" appear>
-    <li v-for="(element, index) in filteredSW_List" :key="`digi-${index}`">
+    <SW_Starship_Card v-for="(starship, index) in starshipStore.filteredList" :key="starship.name" :nom="starship.name" :modele="starship.model"/>
+    <!-- <li v-for="(element, index) in filteredSW_List" :key="element.name">
       <div>
         <p>Nom : {{ element.name }}</p>
         <p>Modèle : {{ element.model }}</p>
@@ -20,37 +21,37 @@
         <p>Longueur : {{ element.length }} mètres</p>
         <p>Nombre maximum de Megalights : {{ element.MGLT }}</p>
         <p>Vitesse maximum en atmosphère : {{ element.max_atmosphering_speed }} miles/h</p>
-        <!-- <p>Apparitions : {{ element.films }}</p> -->
+        <p>Apparitions : {{ element.films }}</p>
         <p>Coût en crédits : {{ element.cost_in_credits }}</p>
       </div>
-    </li>
+    </li> -->
   </TransitionGroup>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { reactive, ref, computed, onMounted } from 'vue'
+import SW_Starship_Card from './components/SW_Starship_Card.vue'
 
-const SW_List = ref([])
-const SW_Starships_List = ref([])
 const filterText = ref('')
 const isLoading = ref(true)
 
-const filteredSW_List = computed(() => 
-  SW_Starships_List.value.filter(element => 
-    element.name.toLowerCase().includes(filterText.value.toLowerCase())
-  )
-)
+const starshipStore = reactive({
+  list: [],
+  filteredList: computed(() => starshipStore.list.filter(starship => starship.name.toLowerCase().includes(filterText.value.toLowerCase())))
+})
 
 onMounted(async () => {
   const data = await fetch('https://www.swapi.tech/api/starships/').then(res => res.json())
-  SW_List.value = data.results
+  
+  //console.log(data.results)
 
-  const promises = SW_List.value.map(async element => {
+  const promises = data.results.map(async element => {
     const detail = await fetch(element.url).then(res => res.json())
     return detail.result.properties
   })
 
-  SW_Starships_List.value = await Promise.all(promises)
+  starshipStore.list = await Promise.all(promises)
+
   isLoading.value = false
 })
 </script>
---
+++ src/components/SW_Starship_Card.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { computed, defineProps } from 'vue'
+
+    const props = defineProps({
+        nom: {
+            type: String,
+            required: true
+        },
+        modele: {
+            type: String,
+            required: true
+        }
+    })
+
+    console.log({ props })
+</script>
+
+<template>
+    <li><span class="bold">{{ nom }}</span> {{ modele }}</li>
+</template>
+
+<style>
+.bold {
+    font-weight: bold;
+}
+</style>
